e85931bb21f49ff6a49031e926abd92e651c052e
Yashwanth Najana <yaswanthnajana@gmail.com>
2024-11-13 11:01:00 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo yum -y update
 sudo yum install -y git
-git clone https://github.com/Yashwanth-najana/fish.git /home/ec2-user/fish
-cd /home/ec2-user/fish
+git clone https://github.com/Yashwanth-najana/fish.git /home/ec2-user/workspace/deploy-slave3/fish
+cd /home/ec2-user/workspace/deploy-slave3/fish/
 sudo yum -y install python3-pip
 pip3 install -r requirements.txt
-nohup python3 -u ./app.py &
+screen -m -d python3 app.py
